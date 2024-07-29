086fbd06a59dbb2ac306ed6d538e0a93cd50eb9d
Mehmed Ayt <Mehmed_ayt@abv.bg>
2024-07-29 12:48:39 +0300

    Add dynamic navigation

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,21 +1,28 @@
 /* eslint-disable react/no-unknown-property */
 
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../context/AuthContext";
 
 export default function Header(){
+  const { isAuthenticated } = useContext(AuthContext);
     return(
         <header>
         <h1><Link className="home" to="/">React Project</Link></h1>
         <nav>
           <Link to="/catalogue">All games</Link>
-          <div id="user">
+          {isAuthenticated 
+          ?<div id="user">
             <Link to="/create">Create Game</Link>
             <Link to="/logout">Logout</Link>
           </div>
-          <div id="guest">
+          :<div id="guest">
             <Link to="/login">Login</Link>
             <Link to="/register">Register</Link>
           </div>
+        }
+          
+          
         </nav>
       </header>
     );
